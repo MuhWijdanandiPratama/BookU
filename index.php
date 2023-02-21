ed9068df5f47b739ef08d40798f853ef4b1fdd9e
ImNizamudin <nisamudin0407@gmail.com>
2023-02-21 15:19:57 +0700

    membuat footer

--- index.php
+++ index.php
@@ -55,9 +55,9 @@ include('affiliate.php');
         <div class="header-2">
             <nav class="nafbar">
                 <a href="index.php">home</a>
-                <a href="#">disclaimer</a>
-                <a href="#">contact</a>
-                <a href="#">DMCA</a>
+                <a href="disclaimer.php">disclaimer</a>
+                <a href="contact.php">contact</a>
+                <a href="dmca.php">DMCA</a>
             </nav>
         </div>
     </header>
@@ -432,6 +432,67 @@ include('affiliate.php');
         </div>
 
     </section>
+
+    <footer style="background: #0a2647;border-top: 2px solid #27AE60;" class="text-white">
+
+        <div class="container text-center mt-5">
+
+            <div>
+                <div class="btn btn-outline-light btn-floating m-1">
+                    <a href="#" style="text-decoration: none;height: 3rem; width: 3rem;line-height: 3rem;font-size: 2rem;color: #fff;" class="fab fa-google"></a>
+                </div>
+                <div class="btn btn-outline-light btn-floating m-1">
+                    <a href="#" style="text-decoration: none;height: 3rem; width: 3rem;line-height: 3rem;font-size: 2rem;color: #fff;" class="fab fa-linkedin"></a>
+                </div>
+                <div class="btn btn-outline-light btn-floating m-1">
+                    <a href="#" style="text-decoration: none;height: 3rem; width: 3rem;line-height: 3rem;font-size: 2rem;color: #fff;" class="fab fa-github"></a>
+                </div>
+            </div>
+
+            <div class="mt-5">
+                <p align="left">
+                    BookU is a digital platform that provides access to a vast collection of books.
+                    These websites allow users to browse through various genres, such as fiction, non-fiction, self-help, and many more.
+                    They offer convenient options to purchase or rent books in both digital and print formats.
+                    Users can also read reviews and ratings from other readers before making a decision.
+                    These websites also provide a platform for authors to showcase their work and engage with readers.
+                    BookU are a great place for book enthusiasts to discover new titles, buy books, and connect with other readers and authors.
+                </p>
+            </div>
+
+            <div class="mt-5">
+
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <a href="index.php" class="text-white text-uppercase text-decoration-none">Home</a>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <a href="disclaimer.php" class="text-white text-uppercase text-decoration-none">Disclaimer</a>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <a href="contact.php" class="text-white text-uppercase text-decoration-none">Contact</a>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <a href="dcma.php" class="text-white text-uppercase text-decoration-none">DMCA</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div class="text-center p-3 mt-5" style="background-color: rgba(0, 0, 0, 0.2);">
+            &copy; 2023 Copyright:
+            <a href="#" class="text-white text-decoration-none">BookU</a>
+        </div>
+
+    </footer>
 
 
 
